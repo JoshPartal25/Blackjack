25109910badfa96d14918f3e6a151c6e021afaf1
joshpartal <joshua_partal25@milton.edu>
2024-05-14 08:59:38 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,9 @@
+public class Card {
+    //suit -> spade, hearts, diamond
+    public String suit;
+    //value -> 1-11
+    public int value;
+    //name -> Ace, 2, 3, ... jack, queen, king
+    public String name;
+
+}
---
+++ src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+
+    //is dealer or not
+    public boolean isDealer;
+
+    //sum of cards
+    public int cardsSum;
+
+    //bust or not
+    public boolean isBust;
+
+    //players' cards
+    public Card[] hand;
+}
